c81b5d36dd0d19b83124c2e283d7cd2c890369cc
DimaE-g-o-r <egorovkog@mail.ru>
2024-09-24 22:43:36 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 2, 5, 'Dima'
+print(immutable_var)
+#immutable_var [0] = 200
+#print(immutable_var)
+mutable_list = [1, 2, 5, 'Dima']
+print(mutable_list)
+mutable_list[0] =  300
+print(mutable_list)
+mutable_list[-1] = 'Kolya'
+print(mutable_list)
